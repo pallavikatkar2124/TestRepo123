cc3da63cb5d08b73e888d711fc610c0e74baeb82
abc <abc@DESKTOP-290HEU1>
2021-04-28 15:43:09 +0530

    test class3

--- AutomationProject/src/testpackage4/Class3.java
+++ AutomationProject/src/testpackage4/Class3.java
@@ -10,7 +10,7 @@ public class Class3 {
 
 	public static void main(String[] args) throws InterruptedException {
 	
-	//System.setProperty("webdriver.chrome.driver","D:\\AUTOMATION\\chromedriver_win32\\chromedriver.exe");
+	System.setProperty("webdriver.chrome.driver","D:\\AUTOMATION\\chromedriver_win32\\chromedriver.exe");
 	System.setProperty("webdriver.chrome.driver","D:\\AUTOMATION\\chromedriver88\\chromedriver.exe");
 	WebDriver driver = new ChromeDriver();
 	driver.manage().window().maximize();
